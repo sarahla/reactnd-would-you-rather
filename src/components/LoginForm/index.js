db23c6afaeec95a73117b51da018c6a8e69d47dd
Sarah Larson <contact@sarahelarson.com>
2020-02-19 23:55:22 -0500

    feat: Connect login form to store with an attempt at using React hooks vs class components

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,27 +1,37 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setAuthedUser } from '../../actions/authedUser';
 
 function LoginForm() {
     const [user, setUser] = useState(null);
+    const dispatch = useDispatch();
+    const state = useSelector( state => {
+        return {
+            ...state,
+            userIds: Object.keys(state.users)
+            }
+        }  
+    );
 
     const handleChange = (e) => {
         const user = e.target.value;
         setUser(user);
-
-        console.log(user);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // TODO: Dispatch Login Event 
-        console.log(`User: ${user}`);
+        dispatch(setAuthedUser(user));
     };
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <select onChange={handleChange}>
                     <option defaultValue disabled>Select a User</option>
-                    <option value="user1">User 1</option>
-                    <option value="user2">User 2</option>
+                    {
+                        state.userIds.map( userId => {
+                            return <option key={userId} value={userId}>{state.users[userId].name}</option>
+                        })
+                    }
                 </select>
                 <button
                     type="submit">
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -9,7 +9,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
           <h1>Would You Rather...?</h1>
         </header>
