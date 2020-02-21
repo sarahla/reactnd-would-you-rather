42a9b062b8406901d2a26c2811819afe215d18b0
Sarah Larson <contact@sarahelarson.com>
2020-02-20 20:50:49 -0500

    Remove unnecessary console logs

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,7 +1,6 @@
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 
 export function receiveQuestions(questions) {
-    //console.log('before receive questions action', questions);
     return {
         type: RECEIVE_QUESTIONS,
         questions
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,6 @@
 import { RECEIVE_USERS } from '../actions/users';
 
 export default function users (state = {}, action) {
-    console.log('users reducer', action.type);
     switch(action.type) {
         case RECEIVE_USERS :
             return {
