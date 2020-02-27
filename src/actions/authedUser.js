e62caf4d1490993249a69875f88fd032fb440d6a
Sarah Larson <contact@sarahelarson.com>
2020-02-26 22:33:37 -0500

    fix: Actually comment out line

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -5,7 +5,7 @@ export function setAuthedUser(id) {
      * Update user in local storage
      * Commenting this functionality out to satisfy #5 of Login Flow in rubric
      */
-    localStorage.setItem('authedUser', id);
+    //localStorage.setItem('authedUser', id);
     return {
         type: SET_AUTHED_USER,
         id
